671432e91f8ce9194909028c006173619b2acaf6
李朋飞_启明 <lpfa0402@163.com>
2020-05-15 09:30:03 +0800

    AjaxButton  MD

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,51 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+h2{
+  font-size: 24px;
+  font-weight: 400;
+  color: #1f2f3d;
+  padding: 20px;
+}
+.cod_wrapper{
+  margin-top: 20px;
+  padding-bottom: 10px;
+  border: #dedede 1px solid;
+  border-radius: 10px;
+  overflow: hidden;
+}
+.cod_top{
+  padding: 24px;
+  border-bottom: #dedede 1px solid;
+}
+.cod_bottom{
+  width: 100%;
+  overflow: hidden;
+}
+.cod_bottom_btn{
+  width: 100%;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 10px;
+}
+.cod_bottom_content{
+  background-color: #384548;
+}
+.Attributes_wrapper{
+  width: 100;
+  margin-top: 10px;
+  overflow: hidden;
+}
+.Attributes_title{
+  width: 100%;
+  font-size: 22px;
+  color: #1f2f3d;
+  padding: 20px;
+}
+.line{
+  height: 116px;
+}
+</style>
--- src/views/Basic/Button/AjaxButton/AjaxButton.vue
+++ src/views/Basic/Button/AjaxButton/AjaxButton.vue
@@ -1,25 +1,110 @@
 <template>
-  <div>
-    <AjaxButton :click="saveHandle" size="small" type="primary">
-      提交
-    </AjaxButton>
+  <div style="padding-bottom: 60px;">
+    <h2>AjaxButton 请求按钮</h2>
+    <div class="cod_wrapper">
+      <div class="cod_top">
+        <!-- 将@click绑定的ajax事件换成 :click绑定 -->
+        <AjaxButton :click="saveHandle" size="medium" type="primary">
+          提交
+        </AjaxButton>
+      </div>
+      <div class="cod_bottom">
+        <div class="cod_bottom_content">
+          <el-collapse-transition>
+            <div v-show="show">
+              <md/>
+            </div>
+          </el-collapse-transition>
+        </div>
+        <div class="cod_bottom_btn">
+          <el-button size="medium" type="text" :icon="show ? 'el-icon-caret-top' : 'el-icon-caret-bottom'" @click="show = !show">{{show ? '隐藏代码' : '显示代码'}}</el-button>
+        </div>
+      </div>
+    </div>
+    <div class="Attributes_wrapper">
+      <div class="Attributes_title">
+        Attributes
+      </div>
+      <el-table
+        :data="AttributesData"
+        size="medium"
+        style="width: 100%">
+        <el-table-column
+          min-width="100px"
+          prop="parameter"
+          label="参数">
+        </el-table-column>
+        <el-table-column
+          min-width="200px"
+          prop="explain"
+          label="说明">
+        </el-table-column>
+        <el-table-column
+          min-width="100px"
+          prop="type"
+          label="类型">
+        </el-table-column>
+        <el-table-column
+          min-width="100px"
+          prop="must"
+          label="必须">
+        </el-table-column>
+        <el-table-column
+          min-width="200px"
+          prop="OptionalValues"
+          label="可选值">
+        </el-table-column>
+        <el-table-column
+          prop="Default"
+          label="默认值">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 <script>
 import '@/mock/AjaxButton.js';
 import { reqPost } from '@/api';
+import md from './README.md';
 export default {
-  name: 'Tabs',
+  name: 'AjaxButtonTest',
+  components: {
+    md
+  },
   data() {
     return {
-      separator: '/',
-      separatorClass: '',
-      replace: false
+      show: false,
+      AttributesData: [
+        {
+          parameter: ':click',
+          explain: '触发请求的绑定',
+          type: 'Function',
+          must: 'true',
+          OptionalValues: '——',
+          Default: '——'
+        },
+        {
+          parameter: 'size',
+          explain: '按钮的尺寸',
+          type: 'String',
+          must: 'false',
+          OptionalValues: 'medium / small / mini',
+          Default: 'mini'
+        },
+        {
+          parameter: 'type',
+          explain: '按钮的类型',
+          type: 'String',
+          must: 'false',
+          OptionalValues: 'primary / success / warning / danger / info / text',
+          Default: '——'
+        }
+      ],
     };
   },
   methods: {
-    async saveHandle(type) {
-      console.log(type);
+    // 异步发送Ajax请求
+    async saveHandle() {
       await reqPost();
     }
   }
@@ -28,13 +113,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.l_checked_wrapper {
-  margin-top: 20px;
-  padding: 10px;
-  border: #dedede 1px solid;
-  border-radius: 10px;
-}
-.l_checked {
-  margin-top: 20px;
-}
 </style>
