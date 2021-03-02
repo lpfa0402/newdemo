9e2fb4937b296f5cf82dd6001c8a0f37df6b177c
李朋飞 <lpfa0402@163.com>
2021-03-02 17:14:37 +0800

    prismjs  样式文件修改

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+/*
+ * @Author: Lpf
+ * @Date: 2021-03-02 16:54:40
+ * @LastEditTime: 2021-03-02 17:13:35
+ * @Description: 
+ */
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/config/use';
 // mackdown代码展示样式
-import 'prismjs/themes/prism.css';
+import '@/assets/css/prism.css';
 // 引入语言切换
 import i18n from './lang';
 Vue.config.productionTip = false;
